20cb164d6193970d378f0aba400b9fb59b173890
mhj1205 <lightnow1205@gmail.com>
2024-08-19 22:10:35 +0900

    day04_0819

---
+++ React/day04/todolist/src/index.js
@@ -0,0 +1,8 @@
+import react from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
